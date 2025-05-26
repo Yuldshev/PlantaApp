54f89719e3083e372a03abc28813c97db36d0bbe
Yuldshev <ikrom921@gmail.com>
2025-05-26 17:47:47 +0500

    Initial Commit

---
+++ Planta/PlantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantaApp.swift
+//  Planta
+//
+//  Created by T on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PlantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
